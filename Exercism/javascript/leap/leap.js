7e7e7e86a4f449c15c47aeb7c777c5ed0209eca9
sam oshaughnessy <samoshaughnessy123@hotmail.com>
2019-03-06 10:10:32 +0800

    leap

---
+++ Exercism/javascript/leap/leap.js
@@ -0,0 +1,22 @@
+//
+// This is only a SKELETON file for the "Leap" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+var Year = function (year) {
+
+    this.year = year;
+};
+
+Year.prototype.isLeap = function () {
+
+    var year = this.year;
+
+    if ((year % 4 == 0 && year % 100 !== 0) ||  year % 400 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+module.exports = Year;
