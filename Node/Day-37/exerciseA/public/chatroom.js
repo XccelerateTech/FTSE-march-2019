e9f256c023daeea0c05828ccfbcb3da62cc3cf3d
sam oshaughnessy <samoshaughnessy123@hotmail.com>
2019-04-24 17:03:00 +0800

    fixes

--- Node/Day-37/exerciseA/public/chatroom.js
+++ Node/Day-37/exerciseA/public/chatroom.js
@@ -1,61 +1,66 @@
-//connect to socket.io
+
+$(()=>{
+
+    alert("Join a room, input a name, then send your messages")
 
 let socket = io.connect('http://localhost:8080');
 
-//set up our variables from the DOM (select the page elements we commonly use)
-//code without jQuery selection
-let message = document.querySelector("#message");
-let btn = document.querySelector("#send");
-let handle = document.querySelector("#handle");
-let output = document.querySelector("#output");
-let echo = document.querySelector("#echo");
-let chatwindow = document.querySelector("#chat-window");
-let joinRoom = document.querySelector("#join")
+let message = $('#message');
+let sendBtn = $('#send');
+let handle = $('#handle')
+let output = $('#output')
+let echo = $('#echo')
+let chatwindow = $('#chat-window')
+let joinRoom =  $('#join')
 
 //submit events to io 
-
 //submit a chat message - press send button
-btn.addEventListener('click', ()=>{
+
+sendBtn.on('click', ()=>{
     socket.emit('chat',{
-        message:message.value,
-        handle:handle.value
-    })
-    message.value =''
+        message : message.val(),
+        handle : handle.val()
+    });
+    message.val('')
 });
 
 //submit a chat message - press enter
-message.addEventListener('keypress', (event)=>{
+
+message.keypress((event)=>{
     if (event.keyCode == 13){
         socket.emit('chat',{
-            message:message.value,
-            handle:handle.value
+            message:message.val(),
+            handle:handle.val()
         })
-        message.value =''
-    }
+        message.val('')
+    };
 });
 
 // if someone is typing emit the isTyping event
-message.addEventListener('keypress', ()=>{
+message.keypress((event)=>{
     if (event.keyCode !== 13){
-        socket.emit('isTyping', handle.value)
-    }
+        socket.emit('isTyping', handle.val())
+    };
 });
+
 
 //listen events from io
 socket.on('chat', (data)=>{
-    echo.innerHTML ='<p> &nbsp</p>'
-    output.innerHTML +='<p><strong>' + data.handle+ ': </strong>' + data.message + '</p>'
-    chatwindow.scrollTop = chatwindow.scrollHeight
+    echo.html('<p> &nbsp</p>');
+    output.append($('<p>').text(`${data.handle} : ${data.message}`));
+    chatwindow.scrollTop = chatwindow.scrollHeight;
 });
 
 socket.on('isTyping', (data)=>{
-    console.log('hello')
-    echo.innerHTML = '<p><em>'+ data +' is typing a message... </em></p>'
+    echo.html('<p><em>'+ data +' is typing a message... </em></p>');
 });
 
 // join a room
-
-joinRoom.addEventListener('click', ()=>{
-    let room = prompt('Which room do you want to join?')
-    socket.emit('subscribe', room)
+joinRoom.on('click', ()=>{
+    let room = prompt('Which room do you want to join?');
+    socket.emit('subscribe', room);
 });
+
+})
+
+// Method Two, create a variable called name, that is equal to a prompt when you load the to capture the value of a name, instead of passing handle.val() pass this variable, name
