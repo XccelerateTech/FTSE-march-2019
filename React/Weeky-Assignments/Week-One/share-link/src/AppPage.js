cddcefe16a2898ce73d5e570e556aac1c77e4b42
sam oshaughnessy <samoshaughnessy123@hotmail.com>
2019-05-24 16:11:17 +0800

    update

--- React/Weeky-Assignments/Week-One/share-link/src/AppPage.js
+++ React/Weeky-Assignments/Week-One/share-link/src/AppPage.js
@@ -1,30 +1,3 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import * as React from 'react';
 import './App.css';
 
@@ -51,8 +24,6 @@ class App extends React.Component{
 
 
     const linksFiltered = this.state.links.filter(link => link.name.toLowerCase().indexOf(lowerSearch) > -1 || link.url.toLowerCase().indexOf(lowerSearch) > -1 )
-
-    // || link.tags.toLowerCase().indexOf(lowerSearch) > -1 
 
     return (
       <div className="container-fluid">
