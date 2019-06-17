fcd5b269f82507d85902ecdb9432a5112170a616
sam oshaughnessy <samoshaughnessy123@hotmail.com>
2019-06-17 16:39:41 +0800

    upload

--- Examples/canvasGithub/assets/javascript/start.js
+++ Examples/canvasGithub/assets/javascript/start.js
@@ -2,17 +2,17 @@
         // currentFunction = new DrawingLine(contextReal,contextDraft);
         currentFunction = new DrawingRectangle(contextReal, contextDraft);
 
-        $('#line').click(()=>{
-            currentFunction = new DrawingLine(contextReal, contextDraft)
-        })
+        // $('#line').click(()=>{
+        //     currentFunction = new DrawingLine(contextReal, contextDraft)
+        // })
 
-        $('#rectangle').click(()=>{
-            currentFunction = new DrawingRectangle(contextReal, contextDraft)
-        })
-        $('#clear').on("click", function () {
-    contextReal.fillStyle = "white";
-    contextReal.fillRect(0, 0, canvasReal.width, canvasReal.height);
-    contextDraft.fillStyle = "white";
-    contextDraft.fillRect(0, 0, canvasReal.width, canvasReal.height);
-    currentFunction = new DrawingRectangle(contextReal, contextDraft);
-})
+        // $('#rectangle').click(()=>{
+        //     currentFunction = new DrawingRectangle(contextReal, contextDraft)
+        // })
+//         $('#clear').on("click", function () {
+//     contextReal.fillStyle = "white";
+//     contextReal.fillRect(0, 0, canvasReal.width, canvasReal.height);
+//     contextDraft.fillStyle = "white";
+//     contextDraft.fillRect(0, 0, canvasReal.width, canvasReal.height);
+//     currentFunction = new DrawingRectangle(contextReal, contextDraft);
+// })
